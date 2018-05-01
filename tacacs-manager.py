5ac85d1be49dc3b9497e7c1dca6640aac4be0d56
jfernatt <fernattj@gmail.com>
2018-04-30 22:43:54 -0400

    Initial commit

---
+++ tacacs-manager.py
@@ -0,0 +1,76 @@
+#!/usr/bin/dev python
+
+import os
+import subprocess as sub
+import readline
+
+def find_os():
+    if os.name == 'nt':
+        clear_screen = 'cls'
+    else
+        clear_screen = 'clear'
+    return clear_screen
+
+def run_menu(menu_dict):
+    return
+
+def get_input():
+    return
+
+def enumerate_menu(my_dict, cls):
+    return
+
+def index_start():
+    return
+
+def manage_svcs():
+    return
+
+def check_socket():
+    return
+
+def start_svc():
+    return
+
+def stop_svc():
+    return
+
+def restart_svc():
+    return
+
+def manage_users():
+    return
+
+def list_user():
+    return
+
+def add_user():
+    return
+
+def del_user():
+    return
+
+def reset_user():
+    return
+
+def manage_accounting():
+    return
+
+def view_account():
+    return
+
+def del_account():
+    return
+
+def back():
+    return
+
+def exit():
+    return
+
+main_menu = {
+1 : ['Something', method]
+}
+
+if __name__ == '__main__'
+    print('running')
